4c6629aa49491fa41112b6b387f978965788f8d0
his027 <60844607+his027@users.noreply.github.com>
2024-10-14 23:29:52 +0800

    Add files via upload

---
+++ tvlogo/pic/update/tokenm.php
@@ -0,0 +1,114 @@
+{
+"use_internal_storage":false,
+"token":"6f2affa6dddb4a408a5877fc062a8d82",
+"open_token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJmOTQyNmQ1Yzg5Zjk0YTA2YmY2MTcxODA0NDliNmQ3NiIsImF1ZCI6Ijc2OTE3Y2NjY2Q0NDQxYzM5NDU3YTA0ZjYwODRmYjJmIiwiZXhwIjoxNzExMzI4NDAwLCJpYXQiOjE3MDM1NTI0MDAsImp0aSI6IjYzNGRkM2U3MWIyZTQ4MTRiZWRkNzA3MDYwZGI3ZjcxIn0.ihwWL49ANcOGB6jq-1DB7hYF-rqJWrfPZpWVnhirelNt0ajT-7AnrdW7Rp3AAUrb4GBWnoP1ONS6o330YTt9uA",
+"open_api_url":"postparam|http://api.extscreen.com/aliyundrive/token",
+"oauth_client_id":"",
+"oauth_client_secret":"",
+"oauth_auth_url":"",
+"oauth_refresh_url":"",
+"is_vip":true,
+"vip_thread_limit":32,
+"vip_thread_limit_night":"19-23=10",
+"vod_flags":"4k|4kz|auto",
+"quark_thread_limit":32,
+"quark_thread_limit_night":"19-23=10",
+"quark_is_guest":false,
+"quark_vip_thread_limit":32,
+"quark_vip_thread_limit_night":"19-23=10",
+"quark_flags":"4kz|auto",
+"uc_thread_limit":0,
+"uc_is_vip":false,
+"uc_vip_thread_limit":0,
+"uc_flags":"4kz|auto",
+"thunder_thread_limit":2,
+"thunder_is_vip":false,
+"thunder_vip_thread_limit":2,
+"thunder_flags":"4k|4kz|auto",
+"aliproxy":"http://www.wya6.cn/tz/aliproxy.php",
+"aliproxy_url":"./lib/aliproxy.gz",
+"proxy":"",
+"danmu":true,
+"quark_danmu":true,
+"quark_cookie":"",
+"uc_cookie":"",
+"thunder_username":"",
+"thunder_password":"",
+"thunder_captchatoken":"",
+"yd_auth":"Basic cGM6MTU3NjgyNzE2NDI6SllOSkNsRW98MXxSQ1N8MTcwNjgxNzc4NTM0OHxlUmZKR2JvRVhjSlN3MDBiRG1vS2ZfNy5nRnMuRXFyWVNJSTBDMjRWLnh3WTYwbzMzYnBMaW5nQWRVMXNOeTJBM1h6dXh2RTk1aXh6ZHNlTnUuOThQWkRRdjQ5bzRnNEtFVHFUcV91WkJ1dy4xazVncVpHU1hlcXNiSDBEQ2xYOGR6d0U5SlhSVUJQaXF0dUlweU43Y2I0cGFSZ09tdk56WGQ4M0IybG9WVU0t",
+"yd_thread_limit":4,
+"yd_flags":"4kz|auto",
+"yd_danmu":true,
+"pikpak_username":"gyj0595@163.com",
+"pikpak_password":"dear0595",
+"pikpak_flags":"4k|auto",
+"pikpak_thread_limit":2,
+"pikpak_vip_thread_limit":2,
+"pikpak_proxy":"",
+"pikpak_proxy_onlyapi":false,
+"pikpak_danmu":true,
+"wgcf_key": "MDx0SyYkQRpshraiKYdP6SjeWqVYjG83LHrjjxFrKFw=",
+"wgcf_key2": "mEO42XlrAWoK6PO4CLgpNPmOElmmEvRB5KuCEOlrxGE=",
+"wgcf_ipport": "162.159.195.199:8742",
+"wgcf_xray_url": "./lib/xray.gz",
+"wgcf_geoip_url":"./lib/geoip.dat.gz",
+"wgcf_json_url": "./lib/wgcf.json",
+"wgcf_vless_id": "",
+"wgcf_vless_optname": "singapore.com:443",
+"wgcf_vless_worker": "edgetunnel-free.pages.dev",
+"wgcf_vless_path": "/?ed=2048",
+"wgcf_vless_protocol": "vless",
+"wgcf_vless_network": "ws",
+"wgcf_vless_tls": false,
+"libxl_url":"./lib/libxl_thunder_sdk.so",
+"youtube_proxy":"proxy",
+"singbox_url":"./lib/sing-box.gz",
+"singbox_subscribe_url":"http://127.0.0.1:9978/file/config.yaml|https://igdux.top/bNNp|https://oss.v2rayse.com/proxies/data/2024-06-23/kxRzdhA.json|https://igdux.top/KXCN|https://igdux.top/brEP",
+"singbox_clash2singbox_url":"./lib/clash2singbox.gz",
+"singbox_template_url":"./lib/singbox.json",
+"singbox_wgcf_json_url":"./lib/wgcf2singbox.json",
+"pan115_cookie":"",
+"pan115_thread_limit":0,
+"pan115_vip_thread_limit":0,
+"pan115_is_vip":false,
+"pan115_flags":"4kz",
+"pan115_speed_limit":0,
+"pan115_speed_limit_mobile":10485760,
+"pan115_auto_delete":true,
+"pan115_delete_code":"",
+"pan_order":"ali|quark|uc|115|yd|thunder|pikpak",
+"tgsearch_url":"./lib/tgsearch",
+"tgsearch_api_id":"",
+"tgsearch_api_hash":"",
+"tgsearch_api_session":"",
+"tgsearch_api_proxy":"proxy",
+"tgsearch_api_url":"http://127.0.0.1:10199/"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
